fae20d9814df9a0684d74dcc4e8a81dc0b0b432b
ganonchenjia_i <ganonchenjia_i@didichuxing.com>
2019-09-10 14:44:55 +0800

    commit FM first step

--- models/FM/FM.py
+++ models/FM/FM.py
@@ -1,100 +1,116 @@
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 
-train_data = pd.read_csv("./")
-train_y = pd.read_csv('./')
-test_x = pd.read_csv("./")
-test_y = pd.read_csv('./')
+from sklearn.base import BaseEstimator, TransformerMixin
 
-# m:训练集大小
-# n:维度大小
-m, n = train_data.shape
-# k: FM的嵌入矩阵大小
-k = 10
+class FM(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_num, embedding_size=8, lr_reg_l1=0, lr_reg_l2=0, fm_reg_l1=0, fm_reg_l2=0, loss="logloss", opt="adam", learning_rate=0.1, epochs=10, batch_size=256, random_seed=2018):
 
-x = tf.placeholder('float', [None, n])
-y = tf.placeholder('float', [None, 1])
+        # 数据参数
+        self.feature_num = feature_num # 特征个数
 
-# 线性部分参数
-w0 = tf.Variable(tf.zeros([1]))
-w = tf.Variable(tf.zeros([n]))
+        # 算法的超参数
+        self.embedding_size = embedding_size # 嵌入矩阵V的大小
+        self.lr_reg_l1 = lr_reg_l1 # LR部分L1正则化系数
+        self.lr_reg_l2 = lr_reg_l2 # LR部分L2正则化系数
+        self.fm_reg_l1 = fm_reg_l1 # FM部分L1正则化系数
+        self.fm_reg_l2 = fm_reg_l2 # FM部分L2正则化系数
 
-# V矩阵参数
-v = tf.variable(tf.random_normal([k, n], mean=0, stddev=0.01))
 
-# 线性模型部分
-# 广播形式对应位置元素相乘
-linear_part = tf.add(w0, tf.reduce_sum(tf.multiply(w, x), 1, keep_dims=True))
 
-# FM增加部分 结果维度：m*1
-fm_cross = 0.5 * tf.reduce_sum(
-    tf.subtract(
-        tf.pow(tf.matmul(x, tf.transpose(v)), 2), tf.matmul(tf.pow(x, 2), tf.transpose(tf.pow(v, 2)))
-    ), axis=1, keep_dims=True
-)
+        # 训练参数
+        self.loss = loss # 损失函数类型
+        self.opt = opt # 优化器类型
+        self.learning_rate = learning_rate # 学习率大小
+        self.epochs = epochs # 训练迭代次数
+        self.batch_size = batch_size # 一个batch数据大小
 
-y_hat = tf.add(linear_part, fm_cross)
+        # 其他参数
+        self.random_seed = random_seed # 随机数种子大小
 
-# 定义损失函数
-# 定义正则化
-lambda_w = tf.constant(0.001, name="lambda_w")
-lambda_v = tf.constant(0.001, name="lambda_v")
 
-l2_norm = tf.reduce_sum(
-    tf.add(
-        tf.multiply(lambda_w, tf.pow(w, 2)),
-        tf.multiply(lambda_v, tf.pow(v, 2)),
-    )
-)
 
-# 平方损失
-error = tf.reduce_mean(tf.square(y - y_hat))
-loss = tf.add(error, l2_norm)
+    def init_graph(self):
+        '''
+        Function: build FM tensorflow calculation graph
+        Returns:
+        '''
+        # 构建tf计算图
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            tf.set_random_seed(self.random_seed) # 设置正则化参数
+            # 设置输入输出
+            train_x = tf.placeholder('float', [None, self.feature_num], name='X') # 输入矩阵维度: m*n,m为数据量大小,n是特征个数
+            label = tf.placeholder('float', [None, 1], name='label') # 目标值维度: m*1
 
-# 定义优化器
-train_op = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+            # part1: 逻辑回归线性部分
+            # 逻辑回归变量
+            w_0 = tf.Variable(tf.zeros([1])) # w0变量,可看做1*1大小
+            w = tf.Variable(tf.zeros[self.feature_num]) # w矩阵，可看做1*n大小
+            # 逻辑回归计算式
+            lr_part = tf.add(w_0, tf.reduce_sum(tf.multiply(w, train_x), axis=1, keep_dims=True)) # 最终输出维度为: m*1
 
-# 定义变量初始化
-init = tf.global_variables_initializer()
+            # part2: FM交叉项部分
+            # V:嵌入矩阵
+            V = tf.Variable(tf.random_normal([self.embedding_size, self.feature_num], mean=0, stddev=0.01)) # 维度为: k*n
+            fm_cross_part = 0.5 * tf.reduce_sum(tf.subtract(tf.pow(tf.matmul(train_x, tf.transpose(V)), 2)), tf.matmul(tf.pow(train_x, 2), tf.transpose(tf.pow(V, 2))), axis=1, keep_dims=True) # 维度为: m*1
 
-epochs = 10
-batch_size = 1000
+            # part3: 合并LR和交叉项的输出
+            output = tf.add(lr_part, fm_cross_part) # 维度为: m*1
 
-def batcher(X_, y_=None, batch_size=-1):
-    n_samples = X_.shape[0]
 
-    if batch_size == -1:
-        batch_size = n_samples
-    if batch_size < 1:
-       raise ValueError('Parameter batch_size={} is unsupported'.format(batch_size))
+            # 定义目标损失
+            obj_loss = tf.losses.log_loss(label, tf.nn.sigmoid(output)) # 默认使用对数损失函数
+            if self.loss == "auc":
+                obj_loss = tf.metrics.auc(label, tf.nn.sigmoid(output)) # AUC
+            elif self.loss == "mse":
+                obj_loss = tf.losses.mean_squared_error(label, tf.nn.sigmoid(output)) # 均方损失函数
+            elif self.loss == "mae":
+                obj_loss = tf.losses.absolute_difference(label, tf.nn.sigmoid(output)) # 绝对损失函数
 
-    for i in range(0, n_samples, batch_size):
-        upper_bound = min(i + batch_size, n_samples)
-        ret_x = X_[i:upper_bound]
-        ret_y = None
-        if y_ is not None:
-            ret_y = y_[i:i + batch_size]
-            yield (ret_x, ret_y)
 
-# 训练
-with tf.Session() as sess:
-    sess.run(init)
+            # 定义正则化损失
+            lr_l1 = tf.constant(self.lr_reg_l1, name="lr_l1")
+            lr_l2 = tf.constant(self.lr_reg_l2, name="lr_l2")
+            fm_l1 = tf.constant(self.fm_reg_l1, name="fm_l1")
+            fm_l2 = tf.constant(self.fm_reg_l2, name="fm_l2")
 
-    for epoch in range(epochs):
-        perm = np.random.permutation(train_data.shape[0])
+            l1_norm = tf.reduce_sum(tf.add(tf.multiply(lr_l1, tf.abs(w)), tf.multiply(fm_l1, tf.abs(V)))) # L1正则化损失函数
+            l2_norm = tf.reduce_sum(tf.add(tf.multiply(lr_l2, tf.pow(w, 2)), tf.multiply(fm_l2, tf.pow(V, 2)))) # L2正则化损失函数
+            norm_loss = tf.add(l1_norm, l2_norm) # 合并正则化损失
 
-        for bX, bY in batcher(train_data[perm], train_y[perm], batch_size):
-            _, t = sess.run([train_op, loss], feed_dict={
-                x: bX.reshape(-1, n), y: bY.reshape(-1, 1)})
-            print(t)
+            loss = tf.add(obj_loss, norm_loss)
 
-        errors = []
-        for bX, bY in batcher(test_x, test_y):
-            errors.append(sess.run(error, feed_dict={
-                x: bX.reshape(-1, n), y: bY.reshape(-1, 1)}))
-            print(errors)
-        RMSE = np.sqrt(np.array(errors).mean())
-        print(RMSE)
+            # 选择优化器
+            if self.opt == "GD": # 梯度下降优化器
+                self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(loss)
+            elif self.opt == "Adam": # Adam优化器
+                self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8).minimize(loss)
+            elif self.opt == "AdaGrad": # AdaGrad优化器
+                self.optimizer = tf.train.AdagradOptimizer(learning_rate=self.learning_rate, initial_accumulator_value=1e-8).minimize(loss)
+            elif self.opt == "Mometum": # 动量下降优化器
+                self.optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate, momentum=0.95).minimize(loss)
+
+            # 初始化saver
+            self.saver = tf.train.Saver()
+            init = tf.global_variables_initializer()
+            self.sess = tf.Session()
+            self.sess.run(init)
+            self.sess.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
